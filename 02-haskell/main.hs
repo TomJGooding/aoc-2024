58541068eb522e31a6c3f5da9a468d1dc0001efa
TomJGooding <101601846+TomJGooding@users.noreply.github.com>
2024-12-04 17:29:42 +0000

    add solution for day 02 part 2

--- 02-haskell/main.hs
+++ 02-haskell/main.hs
@@ -14,6 +14,14 @@ isSafeLevels (x : xs) = checkLevels xs x True True
             | dec && y < prev = checkLevels ys y False True
             | otherwise = False
 
+removeAtIndex :: Int -> [Int] -> [Int]
+removeAtIndex i xs = take i xs ++ drop (i + 1) xs
+
+isSafeDampened :: [Int] -> Bool
+isSafeDampened xs
+    | isSafeLevels xs = True
+    | otherwise = any isSafeLevels [removeAtIndex i xs | i <- [0 .. length xs - 1]]
+
 main = do
     args <- getArgs
     case args of
@@ -21,5 +29,6 @@ main = do
             reports <- map levelsFromReport . lines <$> readFile inputFile
             putStrLn "--- Day 2: Red-Nosed Reports ---"
             printf "Answer for part 1: %d\n" $ length $ filter isSafeLevels reports
+            printf "Answer for part 2: %d\n" $ length $ filter isSafeDampened reports
         _ -> do
             error "Input file not provided"
