10853a187afce88fa97027478fb65664c10b2b0c
TomJGooding <101601846+TomJGooding@users.noreply.github.com>
2024-12-05 12:49:43 +0000

    add solution for day 03 part 2

--- 03-go/main.go
+++ 03-go/main.go
@@ -8,17 +8,25 @@ import (
 )
 
 var mulPattern = regexp.MustCompile(`mul\((\d{1,3}),(\d{1,3})\)`)
+var disabledPattern = regexp.MustCompile(`(?s)don't\(\).*?(?:do\(\)|$)`)
 
 func solvePartOne(input string) int {
     mulMatches := mulPattern.FindAllStringSubmatch(input, -1)
 
     answer := 0
-    for _, mul:= range mulMatches {
+    for _, mul := range mulMatches {
         digit1, _ := strconv.Atoi(mul[1])
         digit2, _ := strconv.Atoi(mul[2])
         answer += digit1 * digit2
     }
 
+    return answer
+}
+
+func solvePartTwo(input string) int {
+    enabledInput := disabledPattern.ReplaceAllString(input, "")
+
+    answer := solvePartOne(enabledInput)
     return answer
 }
 
@@ -36,5 +44,6 @@ func main() {
 
     fmt.Println("--- Day 3: Mull It Over ---")
     fmt.Printf("Answer for part 1: %d\n", solvePartOne(input))
+    fmt.Printf("Answer for part 2: %d\n", solvePartTwo(input))
 
 }
