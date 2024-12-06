b7f7d36456957f3abe1b3bad6f97170371754c35
TomJGooding <101601846+TomJGooding@users.noreply.github.com>
2024-12-06 21:01:40 +0000

    add solution for day 04 part 2

--- 04-cpp/main.cpp
+++ 04-cpp/main.cpp
@@ -73,6 +73,54 @@ int solve_part_one(const vector<string> &wordsearch) {
     return answer;
 }
 
+bool is_x_mas(
+    const vector<string> &wordsearch, const size_t &y, const size_t &x
+) {
+    char center = wordsearch[y + 1][x + 1];
+    if (center != 'A') {
+        return false;
+    }
+
+    char top_left = wordsearch[y][x];
+    char top_right = wordsearch[y][x + 2];
+    char bottom_left = wordsearch[y + 2][x];
+    char bottom_right = wordsearch[y + 2][x + 2];
+
+    if (top_left == 'M' && bottom_right == 'S') {
+        if (top_right == 'M' && bottom_left == 'S') {
+            return true;
+        }
+        if (top_right == 'S' && bottom_left == 'M') {
+            return true;
+        }
+    }
+
+    if (top_left == 'S' && bottom_right == 'M') {
+        if (top_right == 'M' && bottom_left == 'S') {
+            return true;
+        }
+        if (top_right == 'S' && bottom_left == 'M') {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+int solve_part_two(const vector<string> &wordsearch) {
+    int answer = 0;
+
+    for (size_t y = 0; y < wordsearch.size() - 2; y++) {
+        for (size_t x = 0; x < wordsearch[0].length() - 2; x++) {
+            if (is_x_mas(wordsearch, y, x)) {
+                answer += 1;
+            }
+        }
+    }
+
+    return answer;
+}
+
 int main(int argc, char *argv[]) {
     if (argc < 2) {
         cerr << "Input file not provided" << endl;
@@ -83,4 +131,5 @@ int main(int argc, char *argv[]) {
 
     cout << "--- Day 4: Ceres Search ---" << endl;
     cout << "Answer for part 1: " << solve_part_one(wordsearch) << endl;
+    cout << "Answer for part 2: " << solve_part_two(wordsearch) << endl;
 }
